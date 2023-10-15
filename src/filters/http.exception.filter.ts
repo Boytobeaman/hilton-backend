74162965e508148b9fccbb01478e733b5ea9ec74
anthony <luxingchaode@gmail.com>
2023-10-15 12:12:00 +0800

    if called by graphql 401 response.status not function

--- src/filters/http.exception.filter.ts
+++ src/filters/http.exception.filter.ts
@@ -5,22 +5,27 @@ import {
   HttpException,
   LoggerService,
 } from '@nestjs/common';
+import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   constructor(private logger: LoggerService) {}
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
+    const response = ctx.getResponse<Response>();
 
     const status = exception.getStatus();
     this.logger.error(exception.message, exception.stack);
-
-    response.status(status).json({
+    const resdata = {
       statusCode: status,
       timestamp: new Date().toISOString(),
       message: exception.message || exception.name,
       error: exception.name,
-    });
+    };
+
+    // if called by graphql, no response.status available
+    if (response.status) {
+      response.status(status).json(resdata);
+    }
   }
 }
